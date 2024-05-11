787c2aff8a5dd42c665c02e6b5b623ab1f1ddfe7
Amlan Patra <amlanpatra@gmail.com>
2024-05-11 12:47:30 +0530

    files modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="App">
+  <div className=" grid place-content-center h-screen border-[20px] border-red-500">
+    <div>
+      <div className="h-[50px] w-[50px] border-2 border-red-600">Red</div>
+      <div className="h-[50px] w-[50px] border-2 border-red-600">Blue</div>
     </div>
-  );
+    </div>
+  </div> 
+  );      
 }
 
 export default App;
